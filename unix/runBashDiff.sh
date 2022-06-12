fce8d502a69fbed6ceb0e3600628a536d3ed5214
David Georg Reichelt <davidgeorg_reichelt@dagere.de>
2022-06-12 13:54:53 +0200

    Adapt bash execution to common extraction functions

--- unix/runBashDiff.sh
+++ unix/runBashDiff.sh
@@ -1,27 +1,20 @@
-function unzipCommit {
-	commit=$1
-	if [ ! -f $commit.txt ]
-	then
-		echo "Unzipping $commit"
-		unzip ../$commit.zip
-		mv trace.txt $commit.txt
-	fi
-}
+source ../common.sh
+
 
 unzipCommit 414ae6
 unzipCommit afdedc
+prepareData
 
 echo "Finished unzipping"
+
+for size in 10000 50000
+do
+	echo -n "$size "
+	/usr/bin/time --quiet --format="\t%e" ./getDiff.sh $size
+done
 
 for size in {100000..600000..100000}
 do
 	echo -n "$size "
-	if [ ! -d $size ]
-	then
-		
-		mkdir $size
-		head 414ae6.txt -n $size > $size/414ae6.txt
-		head afdedc.txt -n $size > $size/afdedc.txt
-	fi
 	/usr/bin/time --quiet --format="\t%e" ./getDiff.sh $size
 done
