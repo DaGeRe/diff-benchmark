6f76bd2beb4a34aa6b2a8c8631ea5a5c1cec9741
David Georg Reichelt <davidgeorg_reichelt@dagere.de>
2022-06-12 12:24:36 +0200

    Initial commit

---
+++ getDiff.sh
@@ -0,0 +1,2 @@
+size=$1
+diff -y $size/414ae6.txt $size/afdedc.txt > diff_$size.txt
---
+++ runBashDiff.sh
@@ -0,0 +1,27 @@
+function unzipCommit {
+	commit=$1
+	if [ ! -f $commit.txt ]
+	then
+		echo "Unzipping $commit"
+		unzip $commit.zip
+		mv trace.txt $commit.txt
+	fi
+}
+
+unzipCommit 414ae6
+unzipCommit afdedc
+
+echo "Finished unzipping"
+
+for size in {100000..600000..100000}
+do
+	echo -n "$size "
+	if [ ! -d $size ]
+	then
+		
+		mkdir $size
+		head 414ae6.txt -n $size > $size/414ae6.txt
+		head afdedc.txt -n $size > $size/afdedc.txt
+	fi
+	/usr/bin/time --quiet --format="\t%e" ./getDiff.sh $size >> durationBash.csv
+done
